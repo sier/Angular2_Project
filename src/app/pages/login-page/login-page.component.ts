ac439e0ac9d19c75d3415a0956b2905f67ba236c
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-02-15 02:53:29 +0100

    Updated routing

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -27,7 +27,7 @@ export class LoginPageComponent {
     this.afService.loginWithEmail(form.value.email, form.value.password).then((data) => {
       console.log("SIGNED IN WITH EMAIL",data);
       this.isLoading = false;
-      this.router.navigate(['home']);
+      this.router.navigate(['/home']);
     }).catch((error: any) => {
       if (error) {
         this.isLoading = false;
