011ab371359bfbae9e4a5245134078f29c063b64
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-02-15 02:56:00 +0100

    Removed unneccesary functions

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {horsesService} from './horsesService'
 import {fadeInAnimation} from "../../shared/animations/fadeIn.animation";
 import {AngularFire,FirebaseListObservable} from "angularfire2";
 import {AF} from '../../services/af';
@@ -11,31 +10,16 @@ import {AF} from '../../services/af';
   styleUrls: ['./home-page.component.css'],
   animations: [fadeInAnimation]
 })
-export class HomePageComponent implements OnInit{
-  horse: any[] = []; //För funktionen getHorseByID i horsesService ska bytas till horses egentligen som nedan
+export class HomePageComponent {
   horses: FirebaseListObservable<any[]>; //Detta är vad som används just nu för att visa testvärdena då det inte finns några användare kopplade till tabellerna i testdatabasen
 
-  constructor(private angularfire: AngularFire,
-              private horsesService: horsesService,
-              private af: AF){
+  constructor(private angularfire: AngularFire){
 
 
     this.horses = angularfire.database.list('/v1/horses/'); //Detta är vad som används just nu för att visa testvärdena då det inte finns några användare kopplade till tabellerna i testdatabasen
 
 
   }
-  ngOnInit() { //För funktionen getHorseByID i horsesService
-    this.getHorse();
-  }
-
-
-  getHorse() { //För funktionen getHorseByID i horsesService
-    let uid = this.af.uid;
-
-    this.horsesService.getHorseById(uid).subscribe(res => {
-      console.log("SUB:", res);
-      this.horse = res;
-    });
-  }
+  
 
 }
