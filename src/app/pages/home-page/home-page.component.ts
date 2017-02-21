dd1f23515278347cea3a67a3604ff4570c3f8422
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-02-21 15:45:17 +0100

    Update home-page.component.ts

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {fadeInAnimation} from "../../shared/animations/fadeIn.animation";
 import {AngularFire,FirebaseListObservable} from "angularfire2";
 
@@ -9,34 +9,50 @@ import {AngularFire,FirebaseListObservable} from "angularfire2";
   styleUrls: ['./home-page.component.css'],
   animations: [fadeInAnimation]
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
   userinfo: FirebaseListObservable<any[]>; 
   horses: FirebaseListObservable<any[]>;
   trainings: FirebaseListObservable<any[]>;
-  constructor(private angularfire: AngularFire){
+  graphDataTrainings: any[] = [];
 
-
-    this.userinfo = angularfire.database.list('/v1/userinfo/',{
+  
+  constructor(private angularfire: AngularFire) {
+  }
+  
+  ngOnInit() {
+   
+    this.userinfo = this.angularfire.database.list('/v1/userinfo/',{
       query: {
         orderByChild: 'id' ,
         equalTo: '25sZYMr8t9ZZCMtoaCq7NffdIP93'
       }
     }); 
 
-    this.horses = angularfire.database.list('/v1/horses/',{
+    this.horses = this.angularfire.database.list('/v1/horses/',{
       query: {
         orderByChild: 'owner_id' ,
         equalTo: '25sZYMr8t9ZZCMtoaCq7NffdIP93'
       }
     });
 
-    this.trainings = angularfire.database.list('/v1/trainings/',{
+    this.trainings = this.angularfire.database.list('/v1/trainings/',{
       query: {
         orderByChild: 'user' ,
         equalTo: '25sZYMr8t9ZZCMtoaCq7NffdIP93'
       }
     });
+    
+      this.test();
   }
   
+  
+  test() {
+
+    for (let entry of this.trainings) {
+        console.log(entry); // tränings skit visas här
+      
+         this.graphDataTrainings.push(entry.canter.beat)
+    }
+  }
 
 }
